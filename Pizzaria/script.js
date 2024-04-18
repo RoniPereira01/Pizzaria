74c817a5e152ea06db970aff22e42d8fe84cd7dd
Ronilson Pereira <129990000+RoniPereira01@users.noreply.github.com>
2024-04-18 15:39:15 -0300

    Add files via upload

---
+++ Pizzaria/script.js
@@ -0,0 +1,14 @@
+//nesse script ele faz a funcao quando o botao for clicado ele aparece a barra menu
+//na tela.
+let show = true;
+const menuContent = document.querySelector('.content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+})
